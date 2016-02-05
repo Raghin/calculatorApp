ac127a6f00937df4aeb66600e24a5adf3b897cf1
student <student@students-mac.student.georgianc.on.ca>
2016-02-05 10:52:02 -0500

    made number buttons functional

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    //variables
     @IBOutlet weak var numberLabel: UILabel!
+    var calculations: Int = 0
+    var labelString: String = "0"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,50 +25,135 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //button clicked functions
     @IBAction func clearClicked(sender: AnyObject) {
         numberLabel.text =  "0"
+        labelString = "0"
+        calculations = 0
     }
     
     @IBAction func plusclicked(sender: AnyObject) {
+        if calculations == 0{}
     }
     
     @IBAction func minusClicked(sender: AnyObject) {
+        if calculations == 0{}
     }
     
     @IBAction func multiplyClicked(sender: AnyObject) {
+        if calculations == 0{}
     }
     
     @IBAction func dividClicked(sender: AnyObject) {
+        if calculations == 0{}
     }
     
     @IBAction func zeroClicked(sender: AnyObject) {
+        if labelString == "0"{}
+        else{
+            labelString += "0"
+        }
+        
+        numberLabel.text = labelString
     }
 
     @IBAction func oneClicked(sender: AnyObject) {
+        if numberLabel.text == "0"{
+            labelString = "1"
+        }
+        else{
+            labelString += "1"
+        }
+        
+        numberLabel.text = labelString
     }
     
     @IBAction func twoClicked(sender: AnyObject) {
+        if numberLabel.text == "0"{
+            labelString = "2"
+        }
+        else{
+            labelString += "2"
+        }
+        
+        numberLabel.text = labelString
     }
     
     @IBAction func threeClicked(sender: AnyObject) {
+        if numberLabel.text == "0"{
+            labelString = "3"
+        }
+        else{
+            labelString += "3"
+        }
+        
+        numberLabel.text = labelString
     }
     
     @IBAction func fourClicked(sender: AnyObject) {
+        if numberLabel.text == "0"{
+            labelString = "4"
+        }
+        else{
+            labelString += "4"
+        }
+        
+        numberLabel.text = labelString
     }
     
     @IBAction func fiveClicked(sender: AnyObject) {
+        if numberLabel.text == "0"{
+            labelString = "5"
+        }
+        else{
+            labelString += "5"
+        }
+        
+        numberLabel.text = labelString
     }
     
     @IBAction func sixClicked(sender: AnyObject) {
+        if numberLabel.text == "0"{
+            labelString = "6"
+        }
+        else{
+            labelString += "6"
+        }
+        
+        numberLabel.text = labelString
     }
     
     @IBAction func sevenClicked(sender: AnyObject) {
+        if numberLabel.text == "0"{
+            labelString = "7"
+        }
+        else{
+            labelString += "7"
+        }
+        
+        numberLabel.text = labelString
     }
     
     @IBAction func eightClicked(sender: AnyObject) {
+        if numberLabel.text == "0"{
+            labelString = "8"
+        }
+        else{
+            labelString += "8"
+        }
+        
+        numberLabel.text = labelString
     }
     
     @IBAction func nineClicked(sender: AnyObject) {
+        if numberLabel.text == "0"{
+            labelString = "9"
+        }
+        else{
+            labelString += "9"
+        }
+        
+        numberLabel.text = labelString
     }
     
     
