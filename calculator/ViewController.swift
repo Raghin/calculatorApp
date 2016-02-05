fa7ece862af497c70f5553ff4c25db930b648ecf
student <student@students-mac.student.georgianc.on.ca>
2016-02-05 10:18:00 -0500

    created button functions

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clearClicked(sender: AnyObject) {
+        numberLabel.text =  "0"
+    }
+    
+    @IBAction func plusclicked(sender: AnyObject) {
+    }
+    
+    @IBAction func minusClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func multiplyClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func dividClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func zeroClicked(sender: AnyObject) {
+    }
 
+    @IBAction func oneClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func twoClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func threeClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func fourClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func fiveClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func sixClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func sevenClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func eightClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func nineClicked(sender: AnyObject) {
+    }
+    
+    
 }
 
