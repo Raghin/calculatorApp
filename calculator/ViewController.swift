6cd962832a687928f16334302736d445cad8865d
student <student@students-mac.student.georgianc.on.ca>
2016-02-05 13:02:00 -0500

    followed tutorial

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -12,195 +12,55 @@ class ViewController: UIViewController {
 
     //variables
     @IBOutlet weak var numberLabel: UILabel!
-    var calculations: Double = 0
-    var labelString: String = "0"
-    var position: Int = 0
-    var values: [String] = []
+    var first = Double()
+    var second = Double()
+    var isTypingNumber = false
+    var result = Double()
+    var operation = ""
+
     
     //button clicked functions
     @IBAction func clearClicked(sender: AnyObject) {
-        numberLabel.text =  "0"
-        labelString = "0"
-        calculations = 0
+        first = 0
+        second = 0
+        numberLabel.text = "0"
+    }
+    
+    @IBAction func number(sender: AnyObject) {
+        let number = sender.currentTitle
+        if isTypingNumber == true{
+            numberLabel.text = numberLabel.text! + number!!
+        }else {
+            numberLabel.text = number
+        }
+        isTypingNumber = true
+    }
+    
+    @IBAction func operation(sender: AnyObject) {
+        isTypingNumber = false
+        let text: String = numberLabel.text!
+        first = Double(text)!
+        operation = sender.currentTitle!!
     }
     
     @IBAction func calculateClicked(sender: AnyObject) {
-        calculations = Double(labelString)!
+        let text: String = numberLabel.text!
+        second = Double(text)!
+        if operation == "+"{
+            result = first + second
+        }
+        else if operation == "-"{
+            result = first - second
+        }
+        else if operation == "*"{
+            result = first * second
+        }
+        else if operation == "/"{
+            result = first / second
+        }
+        numberLabel.text = "\(result)"
     }
     
-    @IBAction func decimalClicked(sender: AnyObject) {
-        if labelString == "0"{}
-        else{
-            labelString += "."
-            values[position] += "."
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func plusclicked(sender: AnyObject) {
-        if labelString == "0"{}
-        else{
-            position++
-            values[position] = "+"
-            labelString += "+"
-            position++
-            
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func minusClicked(sender: AnyObject) {
-        if labelString == "0"{}
-        else{
-            values[++position] += "-"
-            labelString += "-"
-            position++
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func multiplyClicked(sender: AnyObject) {
-        if labelString == "0"{}
-        else{
-            values[++position] += "*"
-            labelString += "*"
-            position++
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func dividClicked(sender: AnyObject) {
-        if labelString == "0"{}
-        else{
-            values[++position] += "/"
-            labelString += "/"
-            position++
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func zeroClicked(sender: AnyObject) {
-        if labelString == "0"{}
-        else{
-            labelString += "0"
-            values[position] += "0"
-        }
-        
-        numberLabel.text = labelString
-    }
-
-    @IBAction func oneClicked(sender: AnyObject) {
-        if numberLabel.text == "0"{
-            labelString = "1"
-        }
-        else{
-            labelString += "1"
-            values[position] += "1"
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func twoClicked(sender: AnyObject) {
-        if numberLabel.text == "0"{
-            labelString = "2"
-        }
-        else{
-            labelString += "2"
-            values[position] += "2"
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func threeClicked(sender: AnyObject) {
-        if numberLabel.text == "0"{
-            labelString = "3"
-        }
-        else{
-            labelString += "3"
-            values[position] += "3"
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func fourClicked(sender: AnyObject) {
-        if numberLabel.text == "0"{
-            labelString = "4"
-        }
-        else{
-            labelString += "4"
-            values[position] += "4"
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func fiveClicked(sender: AnyObject) {
-        if numberLabel.text == "0"{
-            labelString = "5"
-        }
-        else{
-            labelString += "5"
-            values[position] += "5"
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func sixClicked(sender: AnyObject) {
-        if numberLabel.text == "0"{
-            labelString = "6"
-        }
-        else{
-            labelString += "6"
-            values[position] += "6"
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func sevenClicked(sender: AnyObject) {
-        if numberLabel.text == "0"{
-            labelString = "7"
-        }
-        else{
-            labelString += "7"
-            values[position] += "7"
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func eightClicked(sender: AnyObject) {
-        if numberLabel.text == "0"{
-            labelString = "8"
-        }
-        else{
-            labelString += "8"
-            values[position] += "8"
-        }
-        
-        numberLabel.text = labelString
-    }
-    
-    @IBAction func nineClicked(sender: AnyObject) {
-        if numberLabel.text == "0"{
-            labelString = "9"
-        }
-        else{
-            labelString += "9"
-            values[position] += "9"
-        }
-        
-        numberLabel.text = labelString
-    }
     
     
 }
